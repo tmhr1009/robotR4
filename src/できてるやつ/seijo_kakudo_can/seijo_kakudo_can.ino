c6da6e37947cebf64fb33d689d0cc020f15af047
tmhr1009 <aichitead21107@gmail.com>
2022-07-18 22:29:17 +0900

    add files

--- src/できてるやつ/seijo_kakudo_can/seijo_kakudo_can.ino
+++ src/できてるやつ/seijo_kakudo_can/seijo_kakudo_can.ino
@@ -14,25 +14,22 @@ Pid pid1;
 Pid pid2;
 Pid pidpid;
 
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
-
 FlexCAN CANTransmitter(1000000);
 static CAN_message_t msg;
 static CAN_message_t rxmsg;
 
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28);
+
 long vel[2];
-int canv[2] = {0, 0};
-int flag = 1;//CAN send
+int flag = 0;
 float vx, vy, vt;
 float v = 1.5;
-int u[4] = {};
 int goal = 0;
 int goaltg = 0;
+int u[4] = {};
 int now_dir = 0;//0=北, 1=南, 2=西, 3=東
 int pos_dir[4] = {0, 180, 270, 90};
-int goal_dir = 0;//目標方位
-int flag1 = 0; //角度変更完了後に0
-
+int goal_dir = 0;
 
 void setup() {
   CANTransmitter.begin();
@@ -42,7 +39,6 @@ void setup() {
   pid2.init(6.2, 0.05, 0);
   pidpid.init(150.0, 0, 1);
   Serial.begin(115200);
-  Serial1.begin(100000, SERIAL_8E1);
   MsTimer2::set(2, timerInt);
   MsTimer2::start();
   if (!bno.begin())
@@ -53,36 +49,11 @@ void setup() {
 }
 
 void loop() {
+  flag = 1;
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   int gyro_x = euler.x();
   pidpid.now_value(gyro_x);
-  Serial.print(goal_dir);
-  Serial.print(", ");
-
-  for (int i = 0; i < 4; i++) {
-    u[i] = 0;
-  }
-
-
-  if (gyro_x == goaltg && flag1 == 1) {
-    flag1 = 0;
-    now_dir = goal_dir;
-  }
-
-  if (!(goal_dir == now_dir)) {
-    flag1 = 1;
-    goaltg = pos_dir[goal_dir];
-  }
-
-
-  Serial.print("vel[x,y] : ");
-  Serial.print(vel[0]);
-  Serial.print(", ");
-  Serial.print(vel[1]);
-  Serial.println();
-
-  vx = (int)vel[0] / 1.52;
-  vy = (int)vel[1] / 1.52;
+  goaltg = pos_dir[goal_dir];
 
   if (goaltg > 360)goaltg = goaltg - 360;
   if (goaltg < 0)goaltg = goaltg + 360;
@@ -97,17 +68,26 @@ void loop() {
   }
   vt = goal;
 
-  Serial.print(gyro_x);
+  //goaltg = goaltg + pos_dir[goal_dir];
+
+  vx=vel[0];
+  vy=vel[1];
+  
+  Serial.print("vel[] : ");
+  Serial.print(vel[0]);
   Serial.print(", ");
-  Serial.print(goaltg);
-  Serial.print(", ");
-  Serial.print(goal_dir);
-  Serial.print(", ");
-  Serial.print(now_dir);
-  Serial.print(", ");
+  Serial.print(vel[1]);
+  Serial.println();
+  Serial.print("goaltg : ");
+  Serial.print(pos_dir[goal_dir]);
+  Serial.println();
+  Serial.print("vt : ");
+  Serial.print(vt);
+  Serial.println();
+  Serial.println();
 
   vt = min(max(vt, -500), 500);
-  
+
   u[0] = (int)((vy + vt * 17 / 100) / 0.27557830294);
   u[1] = (int)((sin120 * vx - 0.5 * vy + vt * 17 / 100) / 0.27557830294);
   u[2] = (int)((-sin120 * vx - 0.5 * vy + vt * 17 / 100) / 0.27557830294);
@@ -115,13 +95,6 @@ void loop() {
   u[0] = pid0.pid_out(u[0]);
   u[1] = pid1.pid_out(u[1]);
   u[2] = pid2.pid_out(u[2]);
-
-  Serial.print(u[0]);
-  Serial.print(",");
-  Serial.print(u[1]);
-  Serial.print(",");
-  Serial.println(u[2]);
-  Serial.println("");
 
   msg.id = 0x200;
   msg.len = 8;
@@ -134,7 +107,7 @@ void loop() {
   delay(5);
 }
 
-void timerInt() {}
+void timerInt() {
   if (flag) {
     CANTransmitter.write(msg);
   }
