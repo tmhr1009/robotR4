9119a61b9c51980919bd4bfc2c74e3f23376b70f
tmhr1009 <aichitead21107@gmail.com>
2022-07-18 16:39:21 +0900

    inoutが何故か０にならず暴走

---
+++ src/できてるやつ/arg_arm_mada/arg_arm_mada.ino
@@ -0,0 +1,227 @@
+#include <MsTimer2.h>
+#include <FlexCAN.h>
+#include <Servo.h>
+#include <kinetis_flexcan.h>
+#include "pid.h"
+
+int led = 13;
+
+Servo myservo_ver; //垂直 88(down), 100(up), 95(stop)
+Servo myservo_par; //並行 87(flont), 100(back), 94(stop)
+
+// create CAN object
+FlexCAN CANTransmitter(1000000);
+static CAN_message_t msg;
+static CAN_message_t rxmsg;
+
+Pid pid_ang;
+
+//table_pos[0] テーブルホーム
+int table_pos[7] = {695, 1865, 3035, 4205, 5375, 6545, 7715};
+
+int yaw_tgt_ang = 695; //目標値
+
+int cou = 0;
+int inout = 0;//0=in(拾う), 1=out(出す)
+int flag1 = 0;//input=in switch用
+int place = 0;//次置く場所
+int flag2 = 0;//input=out switch用
+int stage_arm_pick = 0;
+int stage_arm_rele = 0;
+int tim = 0;
+int now_ang = 0;
+
+void setup() {
+  Serial.begin(115200);
+  CANTransmitter.begin();
+  pinMode(led, OUTPUT);
+  pid_ang.init(7.5, 0.0, 0.02);
+  myservo_ver.attach(22);
+  myservo_par.attach(23);
+  MsTimer2::set(2, timerInt);
+  MsTimer2::start();
+  yaw_tgt_ang = table_pos[0];
+}
+
+int u[4] = {0};
+
+void loop() {
+  Serial.println(inout);
+  Serial.println(yaw_tgt_ang);
+  Serial.println(yaw_tgt_ang - 50);
+  Serial.println(yaw_tgt_ang + 50);
+  Serial.println(now_ang);
+
+  if ((yaw_tgt_ang - 50 < now_ang) && (now_ang < yaw_tgt_ang + 50)) {
+    if (place == 7) { //一周したら0に
+      place = 0;
+    }
+    Serial.println(stage_arm_pick);
+    Serial.println(now_ang);
+    Serial.println();
+    //0=in(拾う)
+    if (inout == 0) {
+      Serial.println("11111111111111111");
+      if (flag1 == 0) {
+
+        switch (stage_arm_pick) {
+          case 0:
+            stage_arm_pick++;
+            tim = millis();
+            break;
+          case 1:
+            if (arm_ver_down(tim, millis()) == 0) {
+              stage_arm_pick++;
+              tim = millis();
+              //この次吸盤吸い込み
+            }
+            break;
+          case 2:
+            if (arm_ver_up(tim, millis()) == 0) {
+              stage_arm_pick++;
+              tim = millis();
+            }
+            break;
+          case 3:
+            //置くところまでテーブル回転
+            yaw_tgt_ang = table_pos[place];
+            if (arg_move_end() == 0) {
+              stage_arm_pick++;
+            }
+            break;
+          case 4:
+            flag1 = 1;
+            break;
+        }
+      } else { //拾う→載せるまで終わったあと
+        //テーブルホームに戻る
+        yaw_tgt_ang = table_pos[0];
+        //吸盤脱力
+        stage_arm_pick = 0;
+        flag1 = 0;
+      }
+
+    } //1=out(出す) まだ作ってないCPのみ
+    else if (inout == 1) {
+      if (flag2 == 0) {
+        //出すところの指定
+        yaw_tgt_ang = table_pos[place];
+        switch (stage_arm_rele) {
+          case 0:
+            stage_arm_pick++;
+            tim = millis();
+            break;
+          case 1:
+            if (arm_ver_down(tim, millis()) == 0) {
+              stage_arm_rele++;
+              tim = millis();
+              //この次吸盤吸い込み
+            }
+            break;
+          case 2:
+            if (arm_ver_up(tim, millis()) == 0) {
+              stage_arm_rele++;
+              tim = millis();
+            }
+            break;
+          case 3:
+            flag2 = 1;
+            break;
+        }
+      } else { //出し終わったあと
+        //テーブルホームに戻る
+        yaw_tgt_ang = table_pos[0];
+        //吸盤脱力
+        stage_arm_rele = 0;
+        flag2 = 0;
+      }
+    }
+
+
+
+  }
+
+  msg.id = 0x1FF;
+  msg.len = 8;
+  for (int i = 0; i < 4; i++)
+    u[i] = max(min(pid_ang.pid_out(yaw_tgt_ang), 1000), -1000);
+  for (int i = 0; i < msg.len; i++) {
+    msg.buf[i * 2] = u[i] >> 8;
+    msg.buf[i * 2 + 1] = u[i] & 0x00FF;
+  }
+  digitalWrite(led, !digitalRead(led));
+  cou++;
+  delay(10);
+}
+
+
+void timerInt() {
+  CANTransmitter.write(msg);
+  while ( CANTransmitter.read(rxmsg) ) {
+    if (rxmsg.id == 0x205) {
+      int can_now_ang = rxmsg.buf[0] * 256 + rxmsg.buf[1];
+      pid_ang.now_value(can_now_ang);
+      now_ang = can_now_ang;
+    }
+    if (rxmsg.id == 0x71) {
+      place = rxmsg.buf[2];
+    }
+  }
+}
+
+//アーム降下 ver_down
+int arm_ver_down(int init_tim, int now_tim) {
+  if (now_tim - init_tim < 1000) { //11000
+    Serial.print("void int: ");
+    Serial.println(now_tim - init_tim);
+    myservo_ver.write(88);
+    return 1;
+  } else {
+    myservo_ver.write(94);
+    return 0;
+  }
+}
+
+//アーム上昇 ver_up
+int arm_ver_up(int init_tim, int now_tim) {
+  if (now_tim - init_tim < 1000) {//13500
+    Serial.println(now_tim - init_tim);
+    myservo_ver.write(102);
+    return 1;
+  } else {
+    myservo_ver.write(94);
+    return 0;
+  }
+}
+
+//アーム前進 par_flont
+int arm_par_flont(int init_tim, int now_tim) {
+  if (now_tim - init_tim < 10000) {
+    Serial.println(now_tim - init_tim);
+    myservo_par.write(87);
+    return 1;
+  } else {
+    myservo_par.write(94);
+    return 0;
+  }
+}
+
+//アーム後退 par_back
+int arm_par_back(int init_tim, int now_tim) {
+  if (now_tim - init_tim < 10000) {
+    Serial.println(now_tim - init_tim);
+    myservo_par.write(103);
+    return 1;
+  } else {
+    myservo_par.write(94);
+    return 0;
+  }
+}
+
+int arg_move_end() {
+  if ((table_pos[place] - 50 < now_ang) && (now_ang < table_pos[place] + 50)) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
